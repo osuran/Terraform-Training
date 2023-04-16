7e99f99e896c17a478abd12d49e1f71cea6de056
Osura <osuranew@gmail.com>
2023-04-16 18:31:42 +0800

    example 4

--- example4/security-group.tf
+++ example4/security-group.tf
@@ -8,6 +8,7 @@ resource "aws_security_group" "asia_pacific_sg" {
 
   ingress {
     to_port          = "22"
+    from_port          = "22"
     protocol         = "tcp"
     cidr_blocks      = data.aws_ip_ranges.asia_pacific_ec2.cidr_blocks
   }
