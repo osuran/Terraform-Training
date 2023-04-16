e361ef393922f04904696d342fb5103dda49b128
Osura <osuranew@gmail.com>
2023-04-16 15:25:58 +0800

    add example 2

--- example2/instance.tf
+++ example2/instance.tf
@@ -1,10 +1,3 @@
-provider "aws" {
-
-region     = "ap-southeast-1"
-  access_key = ""
-  secret_key = ""
-}
-
 resource "aws_instance" "vm-terraform" {
   ami           = "ami-04ddf30efb5385f93"
   instance_type = "t2.micro"
