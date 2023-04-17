ae763f61c78fca3220581477bae8c7c719c03c53
Osura <osuranew@gmail.com>
2023-04-17 15:53:57 +0800

    example 5

--- example5/ebs-volume.tf
+++ example5/ebs-volume.tf
@@ -1,11 +1,11 @@
 
 resource "aws_ebs_volume" "ebs-volume" {
-  #availability_zone = aws_instance.vm-terraform.availability_zone.id
+  availability_zone = data.aws_availability_zones.available.names[0]
   size              = 100
 }
 
 resource "aws_volume_attachment" "ebs_attachment" {
-  device_name = "/dev/xvda"
+  device_name = "/dev/xvdb"
   volume_id   = aws_ebs_volume.ebs-volume.id
   instance_id = aws_instance.vm-terraform.id
 }
--- example5/instance.tf
+++ example5/instance.tf
@@ -4,7 +4,17 @@ resource "aws_instance" "vm-terraform" {
   subnet_id = aws_subnet.public-subnet.id
   key_name = aws_key_pair.my-key.id
   vpc_security_group_ids = [aws_security_group.sg_allow_ssh.id]
+availability_zone = data.aws_availability_zones.available.names[0]
 
+
+  root_block_device {
+    volume_size           = "20"
+    volume_type           = "gp2"
+    encrypted             = true
+    delete_on_termination = true
+  }
 }
+
+data "aws_availability_zones" "available" {}
 
 
