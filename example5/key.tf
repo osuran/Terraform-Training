70bb6579a1b9d14009b1fcbba480eaa83f1390a9
Osura <osuranew@gmail.com>
2023-04-17 14:02:36 +0800

    example 5

--- example5/key.tf
+++ example5/key.tf
@@ -1,5 +1,4 @@
 resource "aws_key_pair" "my-key" {
   key_name   = "key-tf"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDOYX+LqVXTmMzv05ikWmF76LDtxzYJGvA7Z7s2l3BwyFaIQ8rWOFvzRyN+akb/BS1eRy9hd1u8D2AGbcFVB6HkkTeEDap5uajo73sccvyN/zPeI2OKNr8h4DTgHSjNT0xFNwEzYB4x1jQHp2f+XfnEKbiOv/i+ERSJAP0GHeXXc3/bkP2csp7nJOwmBTwmvxwlCfzbaa1l2ADTXbba8w6rw003pwbuLNEbJVZnA4yyF6OFTGFJbExTkUNNkf3k+4zxExz9OvlI5C6IwTLg762SWqVgLHmy5hP5gNUebgtXmr7RMIj5DXb1wuMBO6Hn6d3ngV+qe28p5+T+hi+Aif8H osura-sg-rasp
-"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDOYX+LqVXTmMzv05ikWmF76LDtxzYJGvA7Z7s2l3BwyFaIQ8rWOFvzRyN+akb/BS1eRy9hd1u8D2AGbcFVB6HkkTeEDap5uajo73sccvyN/zPeI2OKNr8h4DTgHSjNT0xFNwEzYB4x1jQHp2f+XfnEKbiOv/i+ERSJAP0GHeXXc3/bkP2csp7nJOwmBTwmvxwlCfzbaa1l2ADTXbba8w6rw003pwbuLNEbJVZnA4yyF6OFTGFJbExTkUNNkf3k+4zxExz9OvlI5C6IwTLg762SWqVgLHmy5hP5gNUebgtXmr7RMIj5DXb1wuMBO6Hn6d3ngV+qe28p5+T+hi+Aif8H osura-sg-rasp"
 }
--- example5/vpc.tf
+++ example5/vpc.tf
@@ -21,3 +21,12 @@ resource "aws_eip" "eip" {
   instance = aws_instance.vm-terraform.id
   vpc      = true
 }
+
+resource "aws_route_table" "route_table" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet-gateway.id
+  }
+}
