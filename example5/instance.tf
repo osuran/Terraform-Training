5c6623af0d81e9336b57f3c44cb0106923211d11
Osura <osuranew@gmail.com>
2023-04-17 16:12:56 +0800

    example 5

--- example5/instance.tf
+++ example5/instance.tf
@@ -1,14 +1,15 @@
 resource "aws_instance" "vm-terraform" {
+
   ami           = "${lookup(var.AMIs,var.AWS_REGION)}"
   instance_type = "t2.micro"
   subnet_id = aws_subnet.public-subnet.id
   key_name = aws_key_pair.my-key.id
   vpc_security_group_ids = [aws_security_group.sg_allow_ssh.id]
-availability_zone = data.aws_availability_zones.available.names[0]
+  availability_zone = data.aws_availability_zones.available.names[0]
 
 
   root_block_device {
-    volume_size           = "20"
+    volume_size           = "50"
     volume_type           = "gp2"
     encrypted             = true
     delete_on_termination = true
--- example5/security-group.tf
+++ example5/security-group.tf
@@ -1,12 +1,10 @@
-
-
 resource "aws_security_group" "sg_allow_ssh" {
   vpc_id      = aws_vpc.vpc.id
   name = "ssh allow"
 
   ingress {
     to_port          = "22"
-    from_port          = "22"
+    from_port        = "22"
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
   }
