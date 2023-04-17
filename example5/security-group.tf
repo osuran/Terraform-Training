253cb2594b55aa187d901c17be78be254953d26b
Osura <osuranew@gmail.com>
2023-04-17 14:29:55 +0800

    example 5

--- example5/security-group.tf
+++ example5/security-group.tf
@@ -1,13 +1,14 @@
 
 
 resource "aws_security_group" "sg_allow_ssh" {
+  vpc_id      = aws_vpc.vpc.id
   name = "ssh allow"
 
   ingress {
     to_port          = "22"
     from_port          = "22"
     protocol         = "tcp"
-    cidr_blocks      = "0.0.0.0/0"
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 
 }
