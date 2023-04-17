c4540211c0285a72c7fa94cebf82f3ed19594650
Osura <osuranew@gmail.com>
2023-04-17 13:38:27 +0800

    example 5

--- example5/instance.tf
+++ example5/instance.tf
@@ -2,10 +2,7 @@ resource "aws_instance" "vm-terraform" {
   ami           = "${lookup(var.AMIs,var.AWS_REGION)}"
   instance_type = "t2.micro"
   subnet_id = aws_subnet.public-subnet.id
-
-  
- 
-
+  key_name = aws_key_pair.my-key.id
 
 }
 
