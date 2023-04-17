c5a7f92b5e4aa91814581c9ff420e72acf067e35
Osura <osuranew@gmail.com>
2023-04-17 13:28:30 +0800

    example 5

--- example5/instance.tf
+++ example5/instance.tf
@@ -2,6 +2,8 @@ resource "aws_instance" "vm-terraform" {
   ami           = "${lookup(var.AMIs,var.AWS_REGION)}"
   instance_type = "t2.micro"
   subnet_id = aws_subnet.public-subnet.id
+
+  
  
 
 
--- example5/vpc.tf
+++ example5/vpc.tf
@@ -6,14 +6,18 @@ resource aws_vpc "vpc"{
 
 
 resource "aws_subnet" "public-subnet" {
-  vpc_id     = aws_vpc.example-vpc.id
+  vpc_id     = aws_vpc.vpc.id
   cidr_block = "10.0.1.0/24"
 
 
 }
 
 resource "aws_internet_gateway" "internet-gateway" {
-  vpc_id = aws_vpc.example-vpc.id
+  vpc_id = aws_vpc.vpc.id
 
 }
 
+resource "aws_eip" "eip" {
+  instance = aws_instance.vm-terraform.id
+  vpc      = true
+}
