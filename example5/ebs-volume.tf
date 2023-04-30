bd63f3c1815d6a9a8092c6f31add435abbafaf46
Osura Rathnayake <osura.rathnayake@imperva.com>
2023-04-30 12:23:47 +0800

    AZ association

--- example5/ebs-volume.tf
+++ example5/ebs-volume.tf
@@ -2,7 +2,6 @@
 resource "aws_ebs_volume" "ebs-volume" {
   availability_zone = aws_instance.vm-terraform.availability_zone
   size              = 100
-  delete_on_termination = true
 }
 
 resource "aws_volume_attachment" "ebs_attachment" {
