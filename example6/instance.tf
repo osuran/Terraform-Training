4ad084583ce46881f17c77e4e9463b0cdc25f226
Osura Rathnayake <osura.rathnayake@imperva.com>
2023-04-30 18:00:47 +0800

    add example6

--- example6/instance.tf
+++ example6/instance.tf
@@ -3,9 +3,7 @@
 
 module "instance_module" {
         source = "./modules/instance"
-        ami =  "ami-04ddf30efb5385f92"
-        instance_type = "t2.nano"
-        instance_name = "test"
+
 } 
 
 
