2c66e55a0e9ee026d738f4442cfc06dfcb56c534
Osura Rathnayake <osura.rathnayake@imperva.com>
2023-04-30 18:08:12 +0800

    modify module

--- example6/instance.tf
+++ example6/instance.tf
@@ -3,6 +3,9 @@
 
 module "instance_module" {
         source = "./modules/instance"
+        ami = "${var.ami}"
+        instance_type = "${var.instance_type}"
+        instance_name = "${var.instance_name}"
 
 } 
 
--- example6/modules/instance/vars.tf
+++ example6/modules/instance/vars.tf
@@ -13,7 +13,7 @@ variable "ami" {
 
 variable "instance_type" {
   type          = string
-  default       = "t2-micro"
+  default       = "t2.micro"
 }
 
 variable "instance_name" {
