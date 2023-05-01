189597b6d36efa7bedb73bab98014d370bb60ae1
Osura Rathnayake <osura.rathnayake@imperva.com>
2023-05-01 17:41:30 +0800

    simple changes

--- example6/modules/instance/instance.tf
+++ example6/modules/instance/instance.tf
@@ -1,15 +1,12 @@
 resource "aws_instance" "vm-terraform" {
-  ami           = "${var.ami}"
-  instance_type = "${var.instance_type}"
-  key_name = "${var.key_name}"
+    ami           = "${var.ami}"
+    instance_type = "${var.instance_type}"
+    key_name = "${var.key_name}"
 
-   tags = {
-    Name = "TF Ec2 - ${var.instance_name} "
-  }
+    tags = {
+      Name = "TF Ec2 - ${var.instance_name} "
+    }
   
-
-
-
 }
 
 
