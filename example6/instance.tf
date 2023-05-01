30547c647ff5d6678374ff46f2e1b9f253793375
Osura Rathnayake <osura.rathnayake@imperva.com>
2023-05-01 14:56:15 +0800

    modify modules

--- example6/instance.tf
+++ example6/instance.tf
@@ -1,9 +1,17 @@
 
-module "instance_module" {
+module "instance_module_Hub" {
         source = "./modules/instance"
         ami = "${var.ami}"
         instance_type = "${var.instance_type}"
         instance_name = "${var.instance_name}"
+
+} 
+
+module "instance_module_GW" {
+        source = "./modules/instance"
+        ami = "${var.ami}"
+        instance_type = "${var.instance_type}"
+        instance_name = "GW"
 
 } 
 
