a2213d2fb12385488384020331e81e03725a7394
Osura Rathnayake <osura.rathnayake@imperva.com>
2023-05-09 14:35:04 +0800

    module add

--- example6/instance.tf
+++ example6/instance.tf
@@ -21,7 +21,7 @@ module "ec2_instance" {
   name = "single-instance"
 
   instance_type          = "t2.micro"
-  key_name               = "user1"
+  key_name               = "osura-seoul"
   
   }
 
