a8f1a5188fe4834df2cbf518e0dae20c1f3180b5
Osura Rathnayake <osura.rathnayake@imperva.com>
2023-05-13 15:02:55 +0800

    iam role add

--- example6/instance.tf
+++ example6/instance.tf
@@ -1,3 +1,6 @@
+module "iam-role" {
+        source = "./modules/iam-role"
+}
 
 module "instance_module_Hub" {
         source = "./modules/instance"
@@ -17,10 +20,6 @@ module "instance_module_GW" {
         iam_instance_profile = iam-role.name
 
 } 
-
-module "iam-role" {
-        source = "./modules/iam-role"
-}
 
 module "ec2_instance" {
   source  = "github.com/terraform-aws-modules/terraform-aws-ec2-instance"
