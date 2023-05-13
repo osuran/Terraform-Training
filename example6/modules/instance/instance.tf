9c287d96962af1f4d0f38f7ef20a1a0abd2da160
Osura Rathnayake <osura.rathnayake@imperva.com>
2023-05-13 15:25:48 +0800

    iam role add

--- example6/modules/instance/instance.tf
+++ example6/modules/instance/instance.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "vm-terraform" {
     ami           = "${var.ami}"
     instance_type = "${var.instance_type}"
     key_name = "${var.key_name}"
-    iam_instance_profile = "${var.iam_instance_profile}"
+    //iam_instance_profile = "${var.iam_instance_profile}"
 
     tags = {
       Name = "TF Ec2 - ${var.instance_name} "
