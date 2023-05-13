ff14f6c0a0d66f91345230fc1b06b3989f0764e8
Osura Rathnayake <osura.rathnayake@imperva.com>
2023-05-13 15:30:03 +0800

    iam role add

--- example6/instance.tf
+++ example6/instance.tf
@@ -7,29 +7,10 @@ module "instance_module_Hub" {
         ami = "${var.ami}"
         instance_type = "${var.instance_type}"
         instance_name = "${var.instance_name}"
-        
-  //  iam_instance_profile = iam_role_name.name
 
 } 
 
-module "instance_module_GW" {
-        source = "./modules/instance"
-        ami = "${var.ami}"
-        instance_type = "${var.instance_type}"
-        instance_name = "GW"
-      //  iam_instance_profile = iam_role_name.name
 
-} 
 
-module "ec2_instance" {
-  source  = "github.com/terraform-aws-modules/terraform-aws-ec2-instance"
-
-  name = "single-instance"
-
-  instance_type          = "t2.micro"
-  key_name               = "osura-seoul"
- // iam_instance_profile = iam_role_name.name
-  
-  }
 
 
