4302c335b55ab59aa89279267301d8e29b1ae55d
Osura Rathnayake <osura.rathnayake@imperva.com>
2023-05-13 16:55:30 +0800

    module add

--- example6/instance.tf
+++ example6/instance.tf
@@ -1,4 +1,4 @@
-module "iam_role" {
+module "module_iam_role" {
         source = "./modules/iam-role"
 }
 
@@ -7,7 +7,7 @@ module "instance_module_Hub" {
         ami = "${var.ami}"
         instance_type = "${var.instance_type}"
         instance_name = "${var.instance_name}"
-        iam_instance_profile = "${module.iam_role.name}"
+        iam_instance_profile = "${module.module_iam_role.name}"
 
 } 
 
