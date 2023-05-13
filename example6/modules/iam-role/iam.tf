c13a113e4df761b98e55b40c959dea70fb49f84f
Osura Rathnayake <osura.rathnayake@imperva.com>
2023-05-13 17:31:17 +0800

    module add

--- example6/modules/iam-role/iam.tf
+++ example6/modules/iam-role/iam.tf
@@ -1,27 +1,37 @@
-resource "aws_iam_role" "assume_role" {
-  name = "assume-role-gw"
-  assume_role_policy = <<EOF
+resource "aws_iam_policy" "sts_assume_policy" {
+  name        = "sts-assume-policy"
+  description = "An example IAM policy"
+
+  policy = <<EOF
 {
     "Version": "2012-10-17",
     "Statement": [
         {
+            "Sid": "VisualEditor0",
             "Effect": "Allow",
-            "Action": [
-                "sts:AssumeRole"
-            ],
-            "Principal": {
-                "Service": [
-                    "ec2.amazonaws.com"
-                ]
-            }
+            "Action": "sts:AssumeRole",
+            "Resource": "*"
         }
     ]
 }
 EOF
+}
 
 
+
+
+
+resource "aws_iam_role" "assume_role" {
+  name = "assume-role-gw"
 
 }
+
+resource "aws_iam_role_policy_attachment" "role_policy_attachment" {
+  role       = aws_iam_role.assume_role.name
+  policy_arn = aws_iam_policy.sts_assume_policy.arn
+}
+
+
 
 output "output_name" {
   value = aws_iam_role.assume_role.name
