bd23fb82aaf86190092734e0bd97975307e0a762
Osura Rathnayake <osura.rathnayake@imperva.com>
2023-05-13 18:11:18 +0800

    module add

--- example6/instance.tf
+++ example6/instance.tf
@@ -6,16 +6,15 @@ module "instance_module_Hub" {
         source = "./modules/instance"
         ami = "${var.ami}"
         instance_type = "${var.instance_type}"
-        instance_name = "${var.instance_name}"
+        instance_name = "${var.instance_name} Ec2 from TF local module"
         iam_role = "${module.module_iam_role.output_name}"
 
 } 
 
 
-   module "ec2_instance" {
-  source  = "github.com/terraform-aws-modules/terraform-aws-ec2-instance"
-
-  name = "single-instance"
+module "ec2_instance" {
+        source  = "github.com/terraform-aws-modules/terraform-aws-ec2-instance"
+        name = "${var.instance_name} Ec2 from TF community module"
 
   instance_type          = "t2.micro"
   key_name               = "osura-seoul"
