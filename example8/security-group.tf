aa97e925dd13f94fd599c5109e2d829b207eeb2a
Osura Rathnayake <osura.rathnayake@imperva.com>
2023-05-14 16:19:20 +0800

    module testing example 7

---
+++ example8/security-group.tf
@@ -0,0 +1,33 @@
+
+provider "aws" {
+
+  region     = "ap-northeast-2"
+}
+
+variable "NAME" {
+    default = "Add Your Name"
+}
+
+resource "aws_security_group" "httpd_sg" {
+name = "httpd allow SG ${var.NAME}"
+
+
+  dynamic ingress {
+    for_each = [80,22,443]
+    content{
+          to_port          = ingress.value
+          from_port        = ingress.value
+          protocol         = "tcp"
+          cidr_blocks      = ["0.0.0.0/0"]
+    }
+
+  }
+
+  egress {
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
