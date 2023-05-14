5788d23a59f39b71c9c8e6126f6e318b408841b3
Osura Rathnayake <osura.rathnayake@imperva.com>
2023-05-14 13:10:25 +0800

    module testing

---
+++ example6_dup/modules/instance/instance.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "vm-terraform" {
+    ami           = "${var.ami}"
+    instance_type = "${var.instance_type}"
+    key_name = "${var.key_name}"
+    
+    tags = {
+      Name = "${var.instance_name} "
+    }
+  
+}
+
+
