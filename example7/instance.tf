f7d1e4ed91d66fd9b869cc87b253724d4432dfcf
Osura Rathnayake <osura.rathnayake@imperva.com>
2023-05-14 13:45:09 +0800

    module testing example 7

--- example7/instance.tf
+++ example7/instance.tf
@@ -3,7 +3,10 @@ module "module_iam_role_hub" {
         name = "${var.instance_name}"
 }
 
-
+module "module_iam_role_gw" {
+        source = "./modules/iam-role/iam-role-gw"
+        name = "${var.instance_name}"
+}
 
 
 module "instance_module_Hub" {
@@ -16,7 +19,12 @@ module "instance_module_Hub" {
 
 } 
 
+module "instance_module_gw" {
 
+        source = "./modules/instance"
+        ami = "${var.ami}"
+        instance_type = "${var.instance_type}"
+        instance_name = "${var.instance_name} Ec2 from TF local module"
+        iam_role = "${module.module_iam_role_gw.output_name}"
 
-
-
+}
