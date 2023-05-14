d7a8e1040f39b6da926ed3cb2eaabf3ae4fde254
Osura Rathnayake <osura.rathnayake@imperva.com>
2023-05-15 06:45:18 +0800

    minor changes

--- example7/instance.tf
+++ example7/instance.tf
@@ -25,9 +25,8 @@ module "instance_module_gw" {
         source          = "./modules/instance"
         ami             = "${var.ami}"
         instance_type   = "${var.instance_type}"
-        instance_name   = "${var.server-type == "gw" ? "${var.instance_name} Hub Server" : "${var.instance_name} GW Server ${var.count}"}"
+        instance_name   = "${var.server-type == "gw" ? "${var.instance_name} Hub Server" : "${var.instance_name} GW Server"}"
         iam_role        = "${var.server-type == "gw" ? module.module_iam_role_hub.output_name: module.module_iam_role_gw.output_name}"
         server-type     = "${var.server-type}"
-        count = 2
 } 
 
