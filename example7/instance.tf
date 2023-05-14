18d1e6db858c692369d2394d19df6836d7f21768
Osura Rathnayake <osura.rathnayake@imperva.com>
2023-05-14 14:12:09 +0800

    module testing example 7

--- example7/instance.tf
+++ example7/instance.tf
@@ -14,7 +14,7 @@ module "instance_module_Hub" {
         source = "./modules/instance"
         ami = "${var.ami}"
         instance_type = "${var.instance_type}"
-        instance_name = "${var.server-type == "hub" ? '${var.instance_name} Hub Server' : '${var.instance_name} GW Server'}"
+        instance_name = "${var.server-type == "hub" ? "${var.instance_name} Hub Server" : "${var.instance_name} GW Server"}"
  //       instance_name = "${var.instance_name} Hub Server"
         iam_role = "${module.module_iam_role_hub.output_name}"
 
